d71bc3156861a426fc8d8d677b18652089a76d17
jgonzalez-quincos <neltharion.18@gmail.com>
2024-12-13 13:07:01 -0500

    log in structure

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
